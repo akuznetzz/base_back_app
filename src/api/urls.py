559cf90014f3d7307665d3b95716618027bf43b7
akuznetzz <akuznetzz@yandex.ru>
2024-02-18 15:33:34 +0300

    delete signed urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,16 +1,14 @@
-from django.urls import include
-from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from rest_framework_simplejwt.views import TokenVerifyView
-
 from api.me import MeView
 from api.router import router
-from api.signed_urls import SignedUrlsView
 from auth.views import TokenObtainPairView
 from common.drest_helpers.drest_select import drest_select_enpoint
+from django.urls import include
+from django.urls import path
 from named_views.utils import get_fresh_base_table_view_data
 from named_views.utils.base_table_view_utils import get_fresh_context_view_params_data
 from named_views.utils.base_table_view_utils import get_fresh_default_table_view_descr
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -18,7 +16,6 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/me/', MeView.as_view()),
-    path('auth/sign_url/', SignedUrlsView.as_view()),
     path('get-fresh-base-table-view-data/', get_fresh_base_table_view_data, name='get_fresh_base_table_view_data'),
     path(
         'get-fresh-context-view-params-data/',
